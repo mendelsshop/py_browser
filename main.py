9c3ca16479723f5f58326eb652d7c2c938a9bf92
mendelsshop <82669516+mendelsshop@users.noreply.github.com>
2021-10-07 23:48:59 -0400

    orking...

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cefpython3 import cefpython as cef
 import tkinter
 from tkinter import ttk
 import bookmark
@@ -6,8 +7,10 @@ main_window = ttk.Notebook(root)
 main_window.pack(fill = 'both')
 default_tab = ttk.Frame(main_window)
 main_window.add(default_tab, text = 'New tab')
+search_box = ttk.Entry(default_tab)
+search_box.grid()
 bookmark_bar = ttk.Frame(default_tab)
-bookmark_bar.grid(row = 0)
+bookmark_bar.grid(row = 1)
 bookmarks = bookmark.bookmark_handler()
 for name , url in bookmarks.items():
     bookmark_tuple = (name,url)
@@ -15,4 +18,6 @@ for name , url in bookmarks.items():
     print(name.name)
     name.button = ttk.Button(bookmark_bar,text=name.name)
     name.button.grid(row =0)
+browser = cef.CreateBrowserSync(default_tab, url="https://www.google.com/")
+assert browser
 root.mainloop()

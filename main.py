520bb3e9ea7f2b7c6459bfd32bb3f02a80622f51
mendelsshop <82669516+mendelsshop@users.noreply.github.com>
2021-10-20 20:19:59 -0400

    made adding tabs  semi work besides for chromium itself

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import tkinter
 from tkinter import ttk
 import new_tab
+def binding():
+    tabs[-1].bind('<Expose>',on_add_tab)
 
-def on_add_tab():
-    print('h')
+tabs = []
+def on_add_tab(event):
+    tabs.append(ttk.Frame(main_window))
+    main_window.add(tabs[-1],text='➕')
+    current_tab = len(tabs) -2
+    new_tab.new_Tab(tabs[current_tab])
+    tabs[current_tab].unbind('<Expose>')
+    main_window.tab(tabs[current_tab], text='New tab')
+    binding()
 root = tkinter.Tk()
 main_window = ttk.Notebook(root)
 main_window.pack(fill = 'both')
-tab_1 = ttk.Frame(main_window)
-main_window.add(tab_1, text = 'New tab')
-new_tab.new_Tab(tab_1)
-add_tab = ttk.Frame(main_window)
-main_window.add(add_tab,text='➕')
-add_tab.bind('<FocusIn>',on_add_tab())
+tabs.append(ttk.Frame(main_window))
+main_window.add(tabs[0], text = 'New tab')
+new_tab.new_Tab(tabs[0])
+tabs.append(ttk.Frame(main_window))
+main_window.add(tabs[1],text='➕')
+binding()
 root.mainloop()
